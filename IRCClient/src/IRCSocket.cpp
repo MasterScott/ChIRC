933fad70e285fdb809f5ea6f73d38c16159656db
TotallyNotElite <1yourexperiment@protonmail.com>
2019-06-19 10:50:38 +0200

    More stable DNS resolving
    Thread safety!

--- IRCClient/src/IRCSocket.cpp
+++ IRCClient/src/IRCSocket.cpp
@@ -17,6 +17,17 @@
 #include <fcntl.h>
 #include "IRCSocket.h"
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 #define MAXDATASIZE 4096
 
 bool IRCSocket::Init()
@@ -60,23 +71,61 @@ bool IRCSocket::Init()
     return true;
 }
 
+std::vector<std::string> getipfromhostname(std::string host)
+{
+    struct addrinfo hints, *res;
+    int errcode;
+    char addrstr[100];
+    void *ptr;
+    std::vector<std::string> addresses;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family   = PF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags |= AI_CANONNAME;
+
+    errcode = getaddrinfo(host.c_str(), NULL, &hints, &res);
+    if (errcode != 0)
+        return std::vector<std::string>();
+    while (res)
+    {
+        inet_ntop(res->ai_family, res->ai_addr->sa_data, addrstr, 100);
+
+        switch (res->ai_family)
+        {
+        case AF_INET:
+            ptr = &((struct sockaddr_in *) res->ai_addr)->sin_addr;
+            break;
+        case AF_INET6:
+            ptr = &((struct sockaddr_in6 *) res->ai_addr)->sin6_addr;
+            break;
+        }
+        inet_ntop(res->ai_family, ptr, addrstr, 100);
+        addresses.push_back(addrstr);
+        res = res->ai_next;
+    }
+    return addresses;
+}
+
 bool IRCSocket::Connect(char const *host, int port)
 {
-    hostent *he = gethostbyname(host);
-    if (!he)
+    auto res = getipfromhostname(host);
+
+    if (res.size() == 0)
     {
-        std::cout << "Could not resolve host: " << host << std::endl;
-#ifdef _WIN32
-        WSACleanup();
-#endif
+        std::cout << "IRC: Dns entry not found!" << std::endl;
         return false;
     }
+    std::cout << res[0].c_str() << std::endl;
+
+    char buf[4];
+
+    inet_pton(AF_INET, res[0].c_str(), buf);
 
     sockaddr_in addr;
 
     addr.sin_family = AF_INET;
     addr.sin_port   = htons(port);
-    addr.sin_addr   = *((const in_addr *) he->h_addr);
+    addr.sin_addr   = *((const in_addr *) buf);
     memset(&(addr.sin_zero), '\0', 8);
 
     _connected = true;
